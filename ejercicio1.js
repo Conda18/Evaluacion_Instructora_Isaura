5b124e19e4a16395664747a733d1b33cce6153f6
Marlon Conda Escovar <113036834+Conda18@users.noreply.github.com>
2023-09-07 08:29:20 -0500

    Ejericico uno

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,20 +1,20 @@
 //Elabora un algoritmo que calcule el sueldo promedio de un grupo de empleados y 
 //mostrar el nombre de los empleados que ganan más que el promedio
-let empleado1 = 2000
+let empleado1 = 500
 let empleado2 = 3000
 let empleado3 = 4000
 
 let promedio = (empleado1 + empleado2 + empleado3) / 3;
 
-if(empleado1 => promedio){
+if(empleado1 > promedio){
     console.log("El empleado1 gana mas que el promedio");
 }
 
-if(empleado2 => promedio){
+if(empleado2 > promedio){
     console.log("El empleado2 gana mas que el promedio");
 }
 
-if(empleado3 => promedio){
+if(empleado3 > promedio){
     console.log("El empleado3 gana mas que el promedio");
 }
 
