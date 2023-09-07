d253c5a653042afea3c484f10ae8069bbf70da4d
Marlon Conda Escovar <113036834+Conda18@users.noreply.github.com>
2023-09-07 09:12:07 -0500

    Ejercicio 5

---
+++ ejercicio5.js
@@ -0,0 +1,8 @@
+//Elabore un algoritmo que sume los elementos de un vector 
+//y muestre el resultado. El vector tiene N cantidad de elementos.
+
+let numero = [2, 3, 5, 7];
+
+let suma = numero[0] + numero[1] + numero[2] + numero[3];
+
+console.log("el total es: " + suma);
