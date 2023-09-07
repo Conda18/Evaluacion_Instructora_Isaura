90d537a3248d73420c3dcbf4d276a301e00d209f
Marlon Conda Escovar <113036834+Conda18@users.noreply.github.com>
2023-09-07 09:02:05 -0500

    ejercicio cuatro

---
+++ ejercicio4.js
@@ -0,0 +1,8 @@
+//Realiza un algoritmo que muestre los números primos que hay entre 250 y 550
+
+for(let i = 250; i < dos; i++){
+    if(i % 0 || i){
+        console.log(i);
+    }
+}
+console.log(i)
